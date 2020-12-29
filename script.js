5cf6ce4fa1a57131fea6620f34a332d6109f69a0
Lavstrasni <69418952+Lavstrasni@users.noreply.github.com>
2020-12-29 10:13:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// console
+
+function age(age1, days){
+
+age1 * days
+
+console.log(age)
+
+};
+
+age(15, 365)
+
+// BUTTON
+
+function fun1() {
+
+    var name = prompt('Enter ur usermame')
+
+    document.getElementById('username').innerHTML= name
+}
